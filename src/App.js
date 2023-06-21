be2f35f9b29a2f48c4695c605ff6da96488409bc
Karan Sanghvi <111078325+karansanghvi@users.noreply.github.com>
2023-06-21 14:58:04 +0530

    till form

--- src/App.js
+++ src/App.js
@@ -20,58 +20,58 @@ function App() {
   return (
     <div className="App">
       {/* functional component - props are also included with the variable name */}
-      {/* <Greet name = "Bruce" heroName = "Batman"> 
+      <Greet name = "Bruce" heroName = "Batman"> 
         <p>this is children props</p>
       </Greet>
       <Greet name = "Clark" heroName = "Superman">
         <button>Action</button>
       </Greet>
-      <Greet name = "Diana" heroName = "Wonder Woman"/> */}
+      <Greet name = "Diana" heroName = "Wonder Woman"/>
 
       {/* class component - props are also included with the variable name */}
-      {/* <Welcome name = "Bruce" heroName = "Batman"/>
+      <Welcome name = "Bruce" heroName = "Batman"/>
       <Welcome name = "Clark" heroName = "Superman"/>
-      <Welcome name = "Diana" heroName = "Wonder Woman"/> */}
+      <Welcome name = "Diana" heroName = "Wonder Woman"/>
 
       {/* jsx component */}
-      {/* <Hello/> */}
+      <Hello/>
 
       {/* state */}
-      {/* <Message/> */}
+      <Message/>
 
       {/* setState */}
-      {/* <Counter/> */}
+      <Counter/>
 
       {/* event handling in function components */}
-      {/* <FunctionClick/> */}
+      <FunctionClick/>
 
       {/* event handling in class components */}
-      {/* <ClassClick/> */}
+      <ClassClick/>
 
       {/* binding event handlers */}
-      {/* <EventBind/> */}
+      <EventBind/>
 
       {/* method as props: if we want to access child component with parent component */}
-      {/* <ParentComponent/> */}
+      <ParentComponent/>
 
       {/* conditional rendering */}
-      {/* <UserGreeting/> */}
+      <UserGreeting/>
 
       {/* list rendering */}
-      {/* <NameList/> */}
+      <NameList/>
 
       {/* styling in css */}
 
         {/* APPOACH 1: USING STYLESHEET */}
-        {/* <Stylesheet primary={true}/> */}
+        <Stylesheet primary={true}/>
 
         {/* APPROACH 2: USING INLINE CSS */}
-        {/* <Inline/> */}
+        <Inline/>
 
         {/* APPROACH 3: CSS MODULES */}
-        {/* <h1 className='error'>Error</h1> */}
+        <h1 className='error'>Error</h1>
         {/* in the below code styes is the import statement from the module stylesheet */}
-        {/* <h1 className={styles.success}>Success</h1> */}
+        <h1 className={styles.success}>Success</h1>
       
       {/* form handling */}
       <Form/>
